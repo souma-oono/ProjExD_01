8f0cd8698e5044dc79f16c0c46ef3480bad656b3
c0b22028 <c0b22028c9@edu.teu.ac.jp>
2023-04-18 15:36:14 +0900

    練習問題2

---
+++ pygame_image.py
@@ -0,0 +1,36 @@
+# モジュールをimport
+import pygame as pg
+import sys
+
+# main関数
+def main():
+
+    # ゲームの初期化
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
+
+    # こうかとん画像を読込む
+    kk_img = pg.image.load("ex01/fig/3.png")
+    kk_img = pg.transform.flip(True, False)
+
+    tmr = 0
+
+    # ゲームのループ
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        tmr += 1
+        screen.blit(bg_img, [0, 0])
+
+        pg.display.update()
+        clock.tick(100)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
