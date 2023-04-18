02b3148672733a6f6c71a67a45f803a8d1971385
c0b22028 <c0b22028c9@edu.teu.ac.jp>
2023-04-18 18:13:19 +0900

    演習問題

--- pygame_image.py
+++ pygame_image.py
@@ -9,6 +9,8 @@ def main():
     pg.display.set_caption("はばたけ！こうかとん")
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
+
+    # 背景画像を読込む
     bg_img = pg.image.load("ex01/fig/pg_bg.jpg")
 
     # こうかとん画像を読込む
@@ -18,17 +20,34 @@ def main():
 
     tmr = 0
 
+    # x1:1つ目の画像, x2:2つ目(反転)画像
+    x1 = 0
+    x2 = 1600
+
     # ゲームのループ
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
         tmr += 1
-        screen.blit(bg_img, [-tmr, 0])
-        screen.blit(bg_img, [1600-tmr, 0])
+        x1 -= 1
+        x2 -= 1
+
+        if x1 <= -1600:
+            x1 = 1600
+        
+        if x2 <= -1600:
+            x2 = 1600
+
+        screen.blit(bg_img, [x1, 0])
+        bg_img = pg.transform.flip(bg_img, True, False)
+        screen.blit(bg_img, [x2, 0])
+        bg_img = pg.transform.flip(bg_img, True, False)
+
+        # print("tmr:" + str(tmr) + ", x1:" + str(x1) + ", x2:" + str(x2))
 
         # こうかとん画像を表示
-        screen.blit(kk_imgs[tmr%2], [300, 200])
+        screen.blit(kk_imgs[int((tmr*0.01) % 2)], [300, 200])
 
         pg.display.update()
         clock.tick(100)
